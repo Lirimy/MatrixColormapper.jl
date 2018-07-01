f7f615e4cb214bcb8d387c299bdb4ba0d2fcc264
Lirimy <31124605+Lirimy@users.noreply.github.com>
2018-07-01 15:18:17 +0900

    refactering: combine gif/mp4 struct, etc

--- src/mat2im.jl
+++ src/mat2im.jl
@@ -22,34 +22,38 @@ function matshow(A::AbstractMatrix{T}; cmap=current_colormap()) where T <: Real
     IndirectArray(mappedarray(f, A), cmap)
 end
 
-struct AnimGIF
+file_extension(fn) = lower(Base.Filesystem.splitext(fn)[2][2:end])
+
+struct AnimationFile
     filename::String
 end
 
-struct AnimMP4
-    filename::String
-end
+function Base.show(io::IO, ::MIME"text/html", anim::AnimationFile)
+    ext = file_extension(anim.filename)
 
-function Base.show(io::IO, ::MIME"text/html", anim::AnimGIF)
-    write(io, "<img src=\"$(relpath(anim.filename))?$(rand())>\" />")
-end
-
-function Base.show(io::IO, ::MIME"text/html", anim::AnimMP4)
-    write(io, "<video controls><source src=\"$(relpath(anim.filename))?$(rand())>\" type=\"video/mp4\"></video>")
+    if ext == "mp4"
+        write(io, "<video controls><source src=\"$(relpath(anim.filename))?$(rand())>\" type=\"video/mp4\"></video>")
+    elseif ext == "gif"
+        write(io, "<img src=\"$(relpath(anim.filename))?$(rand())>\" />")
+    else
+        error("Only support mp4/gif: $ext")
+    end
 end
 
 function openanim(f::Function, filename::String="out.mp4")
     filename = abspath(filename)
-    ext = lowercase(Base.Filesystem.splitext(filename)[2][2:end])
+    ext = file_extension(filename)
     
-    if ext == "gif"
+    if ext == "mp4"
+        open(f, `ffmpeg -v 0 -i pipe:0 -pix_fmt yuv420p -y $filename`, "w")
+        return AnimationFile(filename)
+    elseif ext == "gif"
         palette = tempname() * ".bmp"
         save(palette, IndirectArray(reshape(1:256, 16, 16)', current_colormap()))
         open(f, `ffmpeg -v 0 -i pipe:0 -i $palette -lavfi paletteuse=dither=sierra2_4a -y $filename`, "w")
-        return AnimGIF(filename)
-    else # mp4
-        open(f, `ffmpeg -v 0 -i pipe:0 -pix_fmt yuv420p -y $filename`, "w")
-        return AnimMP4(filename)
+        return AnimationFile(filename)
+    else
+        error("Only support mp4/gif: $ext")
     end
 end
 
